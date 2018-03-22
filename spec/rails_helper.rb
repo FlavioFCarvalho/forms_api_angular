ba283ae746979a3a5df274ab2bfaac19e1a46567
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-22 16:49:56 -0300

    Criandos metodos auxiliares de teste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,14 @@ require 'rspec/rails'
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+# Custom json helpers
+config.include Requests::JsonHelpers, type: :request
+# Custom Header helpers
+config.include Requests::HeaderHelpers, type: :request
+
+config.include Devise::Test::ControllerHelpers, type: :controller
+
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -55,7 +63,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include Devise::Test::ControllerHelpers, type: :controller
-
+  
 end
