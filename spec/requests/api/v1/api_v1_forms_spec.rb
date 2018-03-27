981938cea87295fe0173328255da5a30946d7b67
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-27 11:33:26 -0300

    Teste da index

--- spec/requests/api/v1/api_v1_forms_spec.rb
+++ spec/requests/api/v1/api_v1_forms_spec.rb
@@ -1,10 +1,28 @@
-require 'rails_helper'
+describe "GET /forms" do
+    context "With Invalid authentication headers" do
+      it_behaves_like :deny_without_authorization, :get, "/api/v1/forms"
+    end
 
-RSpec.describe "Api::V1::Forms", type: :request do
-  describe "GET /api_v1_forms" do
-    it "works! (now write some real specs)" do
-      get api_v1_forms_index_path
-      expect(response).to have_http_status(200)
+    context "With Valid authentication headers" do
+      before do
+        @user = create(:user)
+        @form1 = create(:form, user: @user)
+        @form2 = create(:form, user: @user)
+
+        get "/api/v1/forms", params: {}, headers: header_with_authentication(@user)
+      end
+
+      it "returns 200" do
+        expect_status(200)
+      end
+
+      it "returns Form list with 2 forms" do
+        expect(json.count).to eql(2)
+      end
+
+      it "returned Forms have right datas" do
+        expect(json[0]).to eql(JSON.parse(@form1.to_json))
+        expect(json[1]).to eql(JSON.parse(@form2.to_json))
+      end
     end
   end
-end
