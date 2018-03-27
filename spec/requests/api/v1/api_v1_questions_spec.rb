5170f9e766da61d44a7b2d65e773c83c7aa6be03
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-27 11:48:30 -0300

    Testes Create

--- spec/requests/api/v1/api_v1_questions_spec.rb
+++ spec/requests/api/v1/api_v1_questions_spec.rb
@@ -1,10 +1,59 @@
-require 'rails_helper'
+describe "POST /questions" do
+  context "With Invalid authentication headers" do
+    it_behaves_like :deny_without_authorization, :post, "/api/v1/questions"
+  end
 
-RSpec.describe "Api::V1::Questions", type: :request do
-  describe "GET /api_v1_questions" do
-    it "works! (now write some real specs)" do
-      get api_v1_questions_index_path
-      expect(response).to have_http_status(200)
+  context "With valid authentication headers" do
+    before do
+      @user = create(:user)
+    end
+
+    context "And with valid params" do
+      before do
+        @form = create(:form, user: @user)
+        @question_attributes = attributes_for(:question)
+        post "/api/v1/questions", params: {question: @question_attributes, form_id: @form.id}, headers: header_with_authentication(@user)
+      end
+
+      it "returns 200" do
+        expect_status(200)
+      end
+
+      it "question are created with correct data" do
+        @question_attributes.each do |field|
+          expect(Question.first[field.first]).to eql(field.last)
+        end
+      end
+
+      it "Returned data is correct" do
+        @question_attributes.each do |field|
+          expect(json[field.first.to_s]).to eql(field.last)
+        end
+      end
+    end
+
+    context "And with invalid params" do
+      before do
+        @other_user = create(:user)
+      end
+
+      context "valid form" do
+        before do
+          @form = create(:form, user: @user)
+        end
+
+        it "returns 400" do
+          post "/api/v1/questions", params: {question: {}, form_id: @form.id}, headers: header_with_authentication(@user)            
+          expect_status(400)
+        end
+      end
+
+      context "invalid form" do
+        it "returns 400" do
+          post "/api/v1/questions", params: {question: {}}, headers: header_with_authentication(@user)            
+          expect_status(404)
+        end
+      end
     end
   end
 end
