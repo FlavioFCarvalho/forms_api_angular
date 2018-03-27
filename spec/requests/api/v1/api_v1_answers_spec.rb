3ffeada756dafbaafbedce033cf3e4039c228bab
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-27 11:53:06 -0300

    Testes index

--- spec/requests/api/v1/api_v1_answers_spec.rb
+++ spec/requests/api/v1/api_v1_answers_spec.rb
@@ -1,10 +1,29 @@
-require 'rails_helper'
+describe "GET /answers" do
+  context "With Invalid authentication headers" do
+    it_behaves_like :deny_without_authorization, :get, "/api/v1/answers"
+  end
 
-RSpec.describe "Api::V1::Answers", type: :request do
-  describe "GET /api_v1_answers" do
-    it "works! (now write some real specs)" do
-      get api_v1_answers_index_path
-      expect(response).to have_http_status(200)
+  context "With Valid authentication headers" do
+    before do
+      @user = create(:user)
+      @form = create(:form, user: @user)
+      @answer1 = create(:answer, form: @form)
+      @answer2 = create(:answer, form: @form)
+
+      get "/api/v1/answers", params: {form_id: @form.id}, headers: header_with_authentication(@user)
+    end
+
+    it "returns 200" do
+      expect_status(200)
+    end
+
+    it "returns Form list with 2 answers" do
+      expect(json.count).to eql(2)
+    end
+
+    it "returned Answers have right datas" do
+      expect(json[0]).to eql(JSON.parse(@answer1.to_json))
+      expect(json[1]).to eql(JSON.parse(@answer2.to_json))
     end
   end
 end
