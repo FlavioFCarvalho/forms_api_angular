fe45e3faaf0f32bc018cce5798c1c08beb4cb6ba
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-27 11:34:19 -0300

    Teste do show

--- spec/requests/api/v1/api_v1_forms_spec.rb
+++ spec/requests/api/v1/api_v1_forms_spec.rb
@@ -26,3 +26,46 @@ describe "GET /forms" do
       end
     end
   end
+
+  describe "GET /forms/:friendly_id" do
+    before do
+      @user = create(:user)
+    end
+
+    context "When form exists" do
+
+      context "And is enable" do
+        before do
+          @form = create(:form, user: @user, enable: true)
+        end
+
+        it "returns 200" do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect_status(200)
+        end
+
+        it "returned Form with right datas" do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect(json).to eql(JSON.parse(@form.to_json))
+        end
+      end
+
+      context "And is unable" do
+        before do
+          @form = create(:form, user: @user, enable: false)
+        end
+
+        it "returns 404" do
+          get "/api/v1/forms/#{FFaker::Lorem.word}", params: {id: @form.friendly_id}, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end
+    end
+
+    context "When form dont exists" do
+      it "returns 404" do
+        get "/api/v1/forms/#{FFaker::Lorem.word}", params: {}, headers: header_with_authentication(@user)
+        expect_status(404)
+      end
+    end
+  end
